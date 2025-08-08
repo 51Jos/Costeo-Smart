2709371ea77a32ec8bf6e11c600b2836d198a016
51Jos <2018102435@ucss.pe>
2025-08-08 16:27:09 -0500

    Update main.dart corregido

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:get/get.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter/foundation.dart';
 
 import 'firebase_options.dart';
 import 'core/utils/initial_bindings.dart';
